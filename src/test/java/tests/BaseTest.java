494f55a93d3d2685f3f09f8e0a8a1dcb4a14d775
Tsukashini <veretig@yandex.by>
2022-04-20 10:03:20 +0300

    Timeouts add

--- src/test/java/tests/BaseTest.java
+++ src/test/java/tests/BaseTest.java
@@ -4,7 +4,10 @@ import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.testng.annotations.AfterMethod;
 import org.testng.annotations.BeforeMethod;
+import staticdata.WebTimeouts;
 import utilities.PropertiesManager;
+
+import java.util.concurrent.TimeUnit;
 
 public class BaseTest {
     WebDriver driver;
@@ -14,6 +17,10 @@ public class BaseTest {
         PropertiesManager propertiesManager = new PropertiesManager();
         System.setProperty("webdriver.chrome.driver", propertiesManager.get("PATH_TO_CHROME_DRIVER"));
         driver = new ChromeDriver();
+        driver.manage().timeouts().setScriptTimeout(WebTimeouts.SCRIPT_TIMEOUT, TimeUnit.SECONDS);
+        driver.manage().timeouts().pageLoadTimeout(WebTimeouts.PAGE_LOAD_TIMEOUT, TimeUnit.SECONDS);
+        driver.manage().timeouts().implicitlyWait(WebTimeouts.IMPLICIT_WAIT_TIMEOUT, TimeUnit.SECONDS);
+
     }
 
     @AfterMethod()
